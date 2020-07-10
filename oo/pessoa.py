7a030614aaa4e390218ce6d339505711c6e07f8f
souwellington <soujorge@yahoo.com.br>
2020-07-09 21:38:05 -0300

    pessoa começando

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,35 +1,5 @@
 class Pessoa:
-    olhos= 3
-    def __init__(self,*filhos, nome = None,idade=35):
-        self.idade = idade
-        self.nome=nome
-        self.filhos = list(filhos)
-
-
-    def cumprimentar(self):
-      return f'Olá {id(self)}'
-
-
-if __name__ == '__main__':
-    wellington= Pessoa(nome='wellington')
-    ana = Pessoa(wellington,nome='ana')
-    print(Pessoa.cumprimentar(ana))
-    print(id(ana))
-    print(ana.cumprimentar())
-    print(ana.nome)
-    print(ana.idade)
-    for filho in ana.filhos:
-        print(filho.nome)
-    ana.sobrenome='Ferreira'
-    del ana.filhos
-    print(ana.__dict__)
-    print(wellington.__dict__)
-    print(ana.olhos)
-    print(wellington.olhos)
-    print(Pessoa.olhos)
-    print(id(Pessoa.olhos), id(wellington.olhos), id(ana.olhos))
-
-
+    pass
 
 
 
